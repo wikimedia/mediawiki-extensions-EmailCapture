b13fff93b5e8ebe8ecb0c0a3d97376f6eaa09469
Zoranzoki21 <zorandori4444@gmail.com>
2020-02-05 03:06:27 +0100

    Convert EmailCapture to use extension registration
    Bug: T241476
    Change-Id: I611d667b25951dd7c4076a97ed522f1bfbef35bb

--- EmailCapture.php
+++ EmailCapture.php
@@ -9,42 +9,17 @@
  * @link https://www.mediawiki.org/wiki/Extension:EmailCapture Documentation
  */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( "This is not a valid entry point to MediaWiki.\n" );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'EmailCapture' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['EmailCapture'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['EmailCaptureAlias'] = __DIR__ . '/EmailCapture.alias.php';
+	wfWarn(
+		'Deprecated PHP entry point used for the EmailCapture extension. ' .
+		'Please use wfLoadExtension() instead, ' .
+		'see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the EmailCapture extension requires MediaWiki 1.29+' );
 }
-
-/* Configuration */
-$wgEmailCaptureSendAutoResponse = true;
-$wgEmailCaptureAutoResponse = array(
-	'from' => $wgPasswordSender,
-	'from-name' => $wgSitename,
-	'subject-msg' => 'emailcapture-response-subject',
-	'body-msg' => 'emailcapture-response-body',
-	'reply-to' => null,
-	'content-type' => null,
-);
-
-// Extension credits that will show up on Special:Version
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'EmailCapture',
-	'author' => 'Trevor Parscal',
-	'version' => '0.4.0',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:EmailCapture',
-	'descriptionmsg' => 'emailcapture-desc',
-);
-
-/* Setup */
-$dir = __DIR__ . '/';
-$wgMessagesDirs['EmailCapture'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['EmailCaptureAlias'] = $dir . 'EmailCapture.alias.php';
-// API
-$wgAutoloadClasses['ApiEmailCapture'] = $dir . 'includes/api/ApiEmailCapture.php';
-$wgAPIModules['emailcapture'] = 'ApiEmailCapture';
-// Schema
-$wgAutoloadClasses['EmailCaptureHooks'] = $dir . 'includes/EmailCaptureHooks.php';
-$wgHooks['LoadExtensionSchemaUpdates'][] = 'EmailCaptureHooks::loadExtensionSchemaUpdates';
-$wgHooks['ParserTestTables'][] = 'EmailCaptureHooks::parserTestTables';
-// Special page
-$wgAutoloadClasses['SpecialEmailCapture'] = $dir . 'includes/specials/SpecialEmailCapture.php';
-$wgSpecialPages['EmailCapture'] = 'SpecialEmailCapture';
